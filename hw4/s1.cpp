9bc26a386bba42511e7c0c18f8f90c0653449080
root <root@BURSTLINKER2.home>
2016-10-26 21:46:45 -0400

    centroid

---
+++ hw4/s1.cpp
@@ -0,0 +1,39 @@
+//Writen by Alan Solitar
+
+#include "image.h"
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace ComputerVisionProjects;
+
+int main(int argc, char **argv) {
+
+	if (argc != 4) {
+		printf("Usage: %s inputfile threshold outputfile\n", argv[0]);
+		return 0;
+	}
+	const string input_file(argv[1]);
+	const int threshold(stoi(argv[2]));
+	const string output_file(argv[3]);
+
+
+	Image an_image;
+	if (!ReadImage(input_file, &an_image)) {
+		cout << "Can't open file " << input_file << endl;
+		return 0;
+	}
+
+	//convert image to binary
+	an_image.ConvertToBinary(threshold);
+	CalculateCentroid(an_image, output_file); 
+
+	
+	
+	if (!WriteImage(output_file, an_image)) {
+		cout << "Can't write to file " << output_file << endl;
+		return 0;
+	}
+	
+}
