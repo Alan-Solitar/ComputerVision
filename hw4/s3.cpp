b167e7dbc770bb117dbb1a6d62cb074c408cb224
root <alsolita1@gmail.com>
2016-11-11 02:22:16 -0500

    changes to s3

--- hw4/s3.cpp
+++ hw4/s3.cpp
@@ -37,8 +37,8 @@ int main(int argc, char **argv) {
 	const int threshold(stoi(argv[6]));
 	const string output_image(argv[7]);
 
-
-	Image an_image1,an_image2,an_image3;
+cout <<step << " "<<threshold;
+	Image an_image1,an_image2,an_image3, needle_map_image;
 	if (!ReadImage(input_image1, &an_image1)) {
 		cout << "Can't open file " << input_image1 << endl;
 		return 0;
@@ -52,35 +52,38 @@ int main(int argc, char **argv) {
 		cout << "Can't open file " << input_image2 << endl;
 		return 0;
 	}
+	const int num_rows = an_image1.num_rows();
+	const int num_columns = an_image1.num_columns();
+	needle_map_image.AllocateSpaceAndSetSize(num_rows, num_columns);
+  	needle_map_image.SetNumberGrayLevels(an_image1.num_gray_levels());
 	
 
 	double matrix[3][3];
 	double resMatrix[3][1];
 	ReadInput(matrix,input_file);
-		for(int i=0;i<3;i++){
-		for(int j =0;j<3;j++){
-			cout <<  matrix[i][j] << " ";
-		}
-		cout<<endl;
-	}
-	
 	InvertMatrix(matrix);
-	auto i1 = CalculateIntensity(an_image1);
-	auto i2 = CalculateIntensity(an_image2);
-	auto i3 = CalculateIntensity(an_image3);
-	double intensities[3] = {i1,i2,i3};
 
-
+	int distance =5;
 	map<pair<int,int>,double[3]> normals;
-	CalculateStuff(an_image1,an_image2,an_image3,threshold,step,normals,matrix);
+	CalculateStuff(an_image1,an_image2,an_image3,step,threshold,normals,matrix);
 	for(auto entry:normals){
-		cout << entry.first.first << " "<<entry.first.second << ": "<< entry.second[0] <<" "<<entry.second[1] << " "<<entry.second[2];
+		int x = entry.first.first;
+		int y  = entry.first.second;
+		int xEnd  = x + entry.second[0]*distance;
+		int yEnd  = y + entry.second[1]*distance;
+		if(x < 100 && y<100)
+		cout << entry.first.first << " "<<entry.first.second << ": "<< entry.second[0] <<" "<<entry.second[1] << " "<<entry.second[2]<<" "<<xEnd << " "<<yEnd <<endl;
+
+		if(xEnd < num_rows && yEnd <num_columns && xEnd > 0 && yEnd >0)
+			DrawLine(x,y,xEnd,yEnd,255,&an_image1);
 	}
 
-	
+	if (!WriteImage(output_image, an_image1)) {
+		cout << "Can't write to file " << output_image << endl;
+		return 0;
+	}
 
 }
-
 void ReadInput(double matrix[3][3], string input_file){
 
 		ifstream reader;
@@ -115,12 +118,18 @@ double CalculateDeterminant(double matrix[3][3]){
 void InvertMatrix(double matrix[3][3]) {
 
 	double determinant = CalculateDeterminant(matrix);
+	determinant = 1/determinant;
 
-	for(int i=0;i<3;i++){
-		for(int j =0;j<3;j++){
-			matrix[i][j] = (1/determinant) * matrix[i][j];
-		}
-	}
+
+	matrix[0][0] =  ((matrix[1][1]*matrix[2][2]) -(matrix[2][1]*matrix[1][2]))*determinant;
+	matrix[1][0] = ((-matrix[0][1]*matrix[2][2])-(matrix[0][2]*matrix[2][1]))*determinant;
+	matrix[2][0] =  ((matrix[0][1]*matrix[1][2])-(matrix[0][2]*matrix[1][1]))*determinant;
+	matrix[0][1] = ((-matrix[1][0]*matrix[2][2])-(matrix[1][2]*matrix[2][0]))*determinant;
+	matrix[1][1] =  ((matrix[0][0]*matrix[2][2])-(matrix[0][2]*matrix[2][0]))*determinant;
+	matrix[2][1] = ((-matrix[0][0]*matrix[1][2])-(matrix[1][0]*matrix[0][2]))*determinant;
+	matrix[0][2] =  ((matrix[1][0]*matrix[2][1])-(matrix[2][0]*matrix[1][1]))*determinant;
+	matrix[1][2] = ((-matrix[0][0]*matrix[2][1])-(matrix[2][0]*matrix[0][1]))*determinant;
+	matrix[2][2] =  ((matrix[0][0]*matrix[1][1])-(matrix[1][0]*matrix[0][1]))*determinant;
 }
 double CalculateIntensity(Image &an_image){
 	vector<double> intensities;
@@ -142,11 +151,12 @@ void CalculateStuff(Image &an_image1,Image &an_image2,Image &an_image3,int step,
 
 	const int num_rows = an_image1.num_rows();
 	const int num_columns = an_image1.num_columns();
+	cout<<step<<endl;
 	int current_step=step;
 	for (size_t i = 0; i < num_rows; ++i) {
     	for (size_t j = 0; j < num_columns; ++j) {
     		if(current_step%step==0){
-
+    			//cout << i <<" " <<j<<endl;
       			int value1 = an_image1.GetPixel(i,j);
       			int value2 = an_image2.GetPixel(i,j);
       			int value3 = an_image3.GetPixel(i,j);
@@ -159,8 +169,6 @@ void CalculateStuff(Image &an_image1,Image &an_image2,Image &an_image3,int step,
       				normals[pt][0] = resultMatrix[0];
       				normals[pt][1] = resultMatrix[1];
       				normals[pt][2] = resultMatrix[2];
-
-
       			}
       		}
       		current_step++;
